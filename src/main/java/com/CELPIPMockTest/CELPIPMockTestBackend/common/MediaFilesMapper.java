846dc21d434e2f6063f7dc4a77c334943e131df6
Sixian <2235127438@qq.com>
2024-10-29 15:42:10 -0700

    update one sql from xml to annotation

--- src/main/java/com/CELPIPMockTest/CELPIPMockTestBackend/common/MediaFilesMapper.java
+++ src/main/java/com/CELPIPMockTest/CELPIPMockTestBackend/common/MediaFilesMapper.java
@@ -1,5 +1,11 @@
 package com.CELPIPMockTest.CELPIPMockTestBackend.common;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
 public interface MediaFilesMapper {
+
+    @Select("SELECT * FROM media_files WHERE id = #{id}")
     MediaFiles getMediaFileById(int id);
 }
